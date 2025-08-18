759342ef1d4ed231eeeebd5fafa94afab5e778ee
SimitSwaad <simitdhamala@gmail.com>
2025-08-18 20:17:37 +1000

    Update footer.js

--- footer.js
+++ footer.js
@@ -19,12 +19,10 @@
 
       </nav>
      <div class="footer-social">
-        <a href="https://www.youtube.com/channel/UCSNQHlvnpKrVDuLtiVAzMHQ">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg">
-            <path d="M23.498 6.186a2.999 2.999 0 0 0-2.115-2.122C19.611 3.5 12 3.5 12 3.5s-7.611 0-9.383.564A2.999 2.999 0 0 0 .502 6.186 31.122 31.122 0 0 0 0 12a31.122 31.122 0 0 0 .502 5.814 2.999 2.999 0 0 0 2.115 2.122c1.772.564 9.383.564 9.383.564s7.611 0 9.383-.564a2.999 2.999 0 0 0 2.115-2.122A31.122 31.122 0 0 0 24 12a31.122 31.122 0 0 0-.502-5.814ZM9.545 15.568V8.432L15.818 12l-6.273 3.568Z"/>
-          </svg>
-        </a>
-      </div>
+       <a href="https://www.youtube.com/channel/your-channel" target="_blank" aria-label="YouTube">
+  <i class="fab fa-youtube"></i>
+</a>
+     </div>
     </div>
     <div class="footer-bottom">
       &copy; ${new Date().getFullYear()} Simit's Swaad. All rights reserved.
