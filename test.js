df1c6eaef6d2b7e68dca1f81b3a169af34f58a5a
SimitSwaad <simitdhamala@gmail.com>
2025-08-21 14:18:28 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// --- Supabase config ---
+// Supabase credentials
+
 const supabaseUrl = 'https://ozdwocrbrojtyogolqxn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZHdvY3Jicm9qdHlvZ29scXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NzE5MzMsImV4cCI6MjA2NjE0NzkzM30.-MAiUtrdza-T2q8POxY-ZcZuZr5QYzFYq5yd-bVYzRQ'; // Replace with your actual anon key
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 // --- DOM elements ---
 const recipesContainer = document.getElementById("recipes-container");
 const searchInput = document.getElementById("search-input");
