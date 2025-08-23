96d8b8b6db1e1ba8b92f1fcbfbf238b5c804aec2
SimitSwaad <simitdhamala@gmail.com>
2025-08-23 10:31:01 +1000

    Update equipment.js

--- equipment.js
+++ equipment.js
@@ -6,7 +6,7 @@ const supabaseUrl = 'https://ozdwocrbrojtyogolqxn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96ZHdvY3Jicm9qdHlvZ29scXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NzE5MzMsImV4cCI6MjA2NjE0NzkzM30.-MAiUtrdza-T2q8POxY-ZcZuZr5QYzFYq5yd-bVYzRQ'; // Replace with your actual anon key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const equipmentContainer = document.getElementById('equipment-container');
+const equipmentContainer = document.getElementById('equipmentGrid');
 
 async function fetchEquipment() {
   try {
